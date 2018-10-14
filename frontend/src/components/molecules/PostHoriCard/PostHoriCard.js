4b071b81ede14c629aabac1274457947a426eb08
bpeak <bpeakcpeak@gmail.com>
2018-10-14 16:15:28 +0900

    PostHoriCard intro webkit clamp 추가

--- frontend/src/components/molecules/PostHoriCard/PostHoriCard.js
+++ frontend/src/components/molecules/PostHoriCard/PostHoriCard.js
@@ -5,8 +5,6 @@ import ProfileImg from '~components/atoms/ProfileImg/ProfileImg'
 import SmallSpinner from '~components/atoms/spinners/SmallSpinner/SmallSpinner'
 //modules
 import history from '~modules/history'
-import getImgSizeFromSrc from '~modules/getImgSizeFromSrc'
-import imgLoader from '~modules/imgLoader'
 //imgs
 import coverDefaultImgSrc from '~assets/nemo.png'
 //styles
@@ -15,51 +13,21 @@ import styles from './PostHoriCard.scss'
 const cx = classNames.bind(styles)
 
 class PostHoriCard extends React.PureComponent {
-    state = {
-        contentsHeight : undefined
-    }
-
-    _setContentsHeight = (contentsHeight) => { this.setState(() => ({ contentsHeight }))}
-    
-    _handleOnCoverImgLoad = () => {
-        const coverImgHeight = this.refs.cover.clientHeight
-        this._setContentsHeight(coverImgHeight)
-    }
-
-    _handleOnWindowResize = () => {
-        const coverImgHeight = this.refs.cover.clientHeight
-        this._setContentsHeight(coverImgHeight)
-    }
-
     _handleOnShowPostClick = () => {
         const post_id = this.props.post._id
         history.push(`/post/${post_id}`)
     }
 
-    componentDidMount(){
-        window.addEventListener('resize', this._handleOnWindowResize)
-    }
-
-    componentWillUnmount(){
-        window.removeEventListener('resize', this._handleOnWindowResize)
-    }
-
     render() {
 
-        const {
-            _handleOnCoverImgLoad,
-            _handleOnShowPostClick
-        } = this
+        const { _handleOnShowPostClick } = this
         const { post } = this.props
-        const { contentsHeight } = this.state
 
         return (
             <div className={cx('PostHoriCard')}>
                 {!post ? <div className={cx('spinner-container')}><SmallSpinner/></div> 
                 :<Fragment>
-                <div className={cx('contents')} style={{ height : contentsHeight || 1}}>
-                    {contentsHeight && 
-                    <Fragment>
+                <div className={cx('contents')}>
                     <div className={cx('title')} onClick={_handleOnShowPostClick}>{post.title}</div>
                     <div className={cx('intro')} onClick={_handleOnShowPostClick}>{post.intro}</div>
                     <div className={cx('authorAndDate')}>
@@ -68,14 +36,11 @@ class PostHoriCard extends React.PureComponent {
                         </div>
                         <span className={cx('nick')}>{post.author.nick}</span>
                     </div>
-                    </Fragment>}
                 </div>
                 <img 
-                    ref="cover"
-                    className={cx('cover')}
-                    onClick={_handleOnShowPostClick}
-                    src={post.coverImgSrc || coverDefaultImgSrc}
-                    onLoad={_handleOnCoverImgLoad}
+                className={cx('cover')}
+                onClick={_handleOnShowPostClick}
+                src={post.coverImgSrc || coverDefaultImgSrc}
                 />
                 </Fragment>}
             </div>
